bb6f29ebf28c27375eaedf9a558b015616681e3a
Pavel Adamovich <42992218+EnneVerden@users.noreply.github.com>
2020-04-29 01:32:18 +0300

    Added loader.

--- src/router.tsx
+++ src/router.tsx
@@ -2,13 +2,14 @@ import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { ROOT_PATH } from "@constants/paths";
+import Loader from "@components/blocks/loader/component";
 
 const AuthPage = lazy(() => import("@components/pages/auth"));
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Suspense fallback="Loading...">
+      <Suspense fallback={<Loader />}>
         <Route exact path={ROOT_PATH} component={AuthPage} />
       </Suspense>
     </Switch>
