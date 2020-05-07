641e327ab773f4df53c56c200056c9957f0faa7f
Pavel Adamovich <42992218+EnneVerden@users.noreply.github.com>
2020-05-07 11:48:36 +0300

    Fixed root saga bug.

--- src/sagas/rootSaga.ts
+++ src/sagas/rootSaga.ts
@@ -1,7 +1,7 @@
 import { all, AllEffect, CallEffect, TakeEffect } from "redux-saga/effects";
 
 import { login } from "./login/login";
-import registration from "./registration/registration";
+import { registration } from "./registration/registration";
 
 export default function* rootSaga(): Generator<
   AllEffect<Generator<TakeEffect | CallEffect>>
