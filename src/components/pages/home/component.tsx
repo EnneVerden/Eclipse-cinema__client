485fa9988e85dd40b07f88ac48927deceaf88353
Pavel Adamovich <42992218+EnneVerden@users.noreply.github.com>
2020-05-07 12:34:27 +0300

    Added home page.

---
+++ src/components/pages/home/component.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import useStyles from "./styles";
+import Header from "components/blocks/header";
+
+const HomePage = () => {
+  const styles = useStyles();
+
+  return <Header />;
+};
+
+export default HomePage;
--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,18 @@
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import { ROOT_PATH } from "constants/paths";
+import { AUTH_PATH, HOME_PATH } from "constants/paths";
 import PageLoader from "components/blocks/page-loader";
 
 const AuthPage = lazy(() => import("components/pages/auth"));
+const HomePage = lazy(() => import("components/pages/home"));
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Suspense fallback={<PageLoader />}>
-        <Route exact path={ROOT_PATH} component={AuthPage} />
+        <Route exact path={AUTH_PATH} component={AuthPage} />
+        <Route exact path={HOME_PATH} component={HomePage} />
       </Suspense>
     </Switch>
   </BrowserRouter>
